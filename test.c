84f4273e574c6a8a92cbac238eae75c2733900f2
UH5555 <zhuhao162@163.com>
2022-04-14 22:06:58 +0800

    0414\字符串函数的使用与剖析（2）

--- test.c
+++ test.c
@@ -63,30 +63,107 @@
 //	return 0;
 //}
 
-my_strcat(char* dest, const char* src)
-{
-	char* ret = dest;
-	assert(dest != NULL);
-	assert(src);
-	//1.找到目的字符串的’\0'
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	//2.追加
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	return ret;
-}
+//my_strcat(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest != NULL);
+//	assert(src);
+//	//1.找到目的字符串的’\0'
+//	while (*dest != '\0')
+//	{
+//		dest++;
+//	}
+//	//2.追加
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[30] = "hello\0xxxxxxxxxx";
+//	char arr2[] = "world";
+//	my_strcat(arr1, arr2);
+//	//strcat(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//> 1
+//	//= 0
+//	//< -1
+//	char* p1 = "abcdef";
+//	char* p2 = "sqwer";
+//	//int ret = strcmp(p1, p2);
+//	//printf("%d\n", ret);//-1
+//	if (strcmp(p1, p2) > 0)
+//		printf("p1>p2\n");
+//	else if (strcmp(p1, p2) == 0)
+//		printf("p1==p2\n");
+//	else
+//		printf("p1<p2\n");
+//	return 0;
+//}
+
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	//比较
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//		{
+//			return 0;
+//		}
+//		str1++;
+//		str2++;
+//	}
+//	/*if (*str1 > *str2)
+//		return 1;
+//	else
+//		return -1;*/
+//	return (*str1 - *str2);
+//}
+//
+//int main()
+//{
+//	char* p1 = "abcdef";
+//	char* p2 = "abqwe";
+//	int ret = my_strcmp(p1, p2);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+////字符串长度不受限制
+//int main()
+//{
+//	char arr1[5] = "abc";
+//	char arr2[] = "hello bit";
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[10] = "abcdefgh";
+//	char arr2[] = "hello world";
+//	strcpy(arr1, arr2, 6);// \0不会拷贝，如果个数不够，则追加\0
+//	return 0;
+//}
 
 int main()
 {
-	char arr1[30] = "hello\0xxxxxxxxxx";
+	char arr1[30] = "hello\0xxxxxxxxx";
+	char arr3[30] = "hello\0xxxxxxxxx";
 	char arr2[] = "world";
-	my_strcat(arr1, arr2);
-	//strcat(arr1, arr2);
+	strncat(arr1, arr2, 3);//hellowor\0
+	strncat(arr3, arr2, 8);//helloworld\0
 	printf("%s\n", arr1);
+	printf("%s\n", arr3);
 	return 0;
 }
