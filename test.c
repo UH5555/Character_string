73d8572e02ef13656fce5b26a1cb7cdc8c1ffa2d
UH5555 <zhuhao162@163.com>
2022-04-15 20:25:16 +0800

    0415\字符串函数的使用与剖析（3）

--- test.c
+++ test.c
@@ -156,14 +156,68 @@
 //	return 0;
 //}
 
-int main()
-{
-	char arr1[30] = "hello\0xxxxxxxxx";
-	char arr3[30] = "hello\0xxxxxxxxx";
-	char arr2[] = "world";
-	strncat(arr1, arr2, 3);//hellowor\0
-	strncat(arr3, arr2, 8);//helloworld\0
-	printf("%s\n", arr1);
-	printf("%s\n", arr3);
-	return 0;
-}
+//int main()
+//{
+//	char arr1[30] = "hello\0xxxxxxxxx";
+//	char arr3[30] = "hello\0xxxxxxxxx";
+//	char arr2[] = "world";
+//	strncat(arr1, arr2, 3);//hellowor\0
+//	strncat(arr3, arr2, 8);//helloworld\0
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr3);
+//	return 0;
+//}
+
+//int main()
+//{
+//	const char* p1 = "abcdef";
+//	const char* p2 = "abcqwer";
+//	int ret = strncmp(p1, p2, 3);
+//	int ret2 = strncmp(p1, p2, 4);
+//	printf("%d\n", ret);
+//	printf("%d\n", ret2);
+//	//strncmp - 字符串比较
+//	return 0;
+//}
+
+////strstr - 查找字符串
+//char* my_strstr(const char* p1, const char* p2)
+//{
+//	assert(p1 != NULL);
+//	assert(p2 != NULL);
+//	char* s1 = p1;
+//	char* s2 = p2;
+//	char* cur = (char*)p1;
+//	if (*p2 == '\0')
+//		return (char*)p1;
+//	while (*cur)
+//	{
+//		s1 = cur;
+//		s2 = p2;
+//		while ((*s2) && !(*s1-*s2)&&(*s1))
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')
+//		{
+//			return cur;//找到子串
+//		}
+//		cur++;
+//	}
+//	return NULL;
+//	//找不到子串
+//}
+//
+//int main()
+//{
+//	char* p1 = "abcdefghi";
+//	char* p2 = "def";
+//	//char* ret = strstr(p1, p2);
+//	char* ret = my_strstr(p1, p2);
+//	if (ret == NULL)
+//		printf("子串不存在");
+//	else
+//		printf("%s\n", ret);
+//	return 0;
+//}
